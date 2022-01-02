0cda8884e00bbf6dbcfb787215b05dcfecc39f8e
Michael DeDreu <mdedreu21@gmail.com>
2022-01-01 23:26:46 -0500

    Retry logic for db for db connection

--- index.js
+++ index.js
@@ -1,12 +1,27 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const dbConfig = require('./config/config')
 
 const app = express()
 const port = process.env.PORT || 3000
 
-mongoose.connect('mongodb://root:docker123@db/?authSource=admin')
-    .then(() => console.log('Successfully connected to the database'))
-    .catch((err) => console.log(`Unexpected error connecting to db: ${err}`))
+
+const dbUrl = `mongodb://${dbConfig.MONGO_USER}:${dbConfig.MONGO_PASSWORD}@${dbConfig.MONGO_IP}:${dbConfig.MONGO_PORT}/?authSource=admin`
+
+// Attempt to connect to database up to 20 times with exponential backoff
+const connectWithRetry = (retry = 0, delay = 100) => {
+    mongoose.connect(dbUrl)
+        .then(() => console.log('Successfully connected to the database'))
+        .catch((err) => {
+            console.log(`Retry: ${retry} Unexpected error connecting to db: ${err}`)
+
+            if (retry < 20) setTimeout(connectWithRetry(retry++, delay ** 2), delay)
+        })
+}
+
+connectWithRetry()
+
+
 
 app.get('/', (req, res) => {
     res.send("<h1>Hey!! Updated again</h1>")
