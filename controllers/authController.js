0743fb1b9854d9b4d4d49caaaabae4ff2d2ecaf9
Michael DeDreu <mdedreu21@gmail.com>
2022-01-02 12:02:43 -0500

    Basic login functionality

---
+++ controllers/authController.js
@@ -0,0 +1,56 @@
+const User = require('../models/userModel')
+const bcrypt = require('bcryptjs')
+
+exports.signUp = async (req, res) => { 
+    try {
+        const { username, password } = req.body
+        const hashedPassword = await bcrypt.hash(password, 12)
+
+        const newUser = await User.create({
+            username,
+            password: hashedPassword
+        })
+
+        res.status(201).json({
+            status: 'success',
+            data: newUser
+        })
+    } catch (err) {
+        res.status(500).json({
+            status: 'failed',
+            error: err
+        })
+    }
+}
+
+
+exports.login = async (req, res) => {
+    try {
+        const { username, password } = req.body
+        
+        const user = await User.findOne({username})
+        if (!user) {
+            return res.status(404).json({
+                status: 'failed',
+                error: 'User was not found'
+            })
+        }
+
+        const matched = await bcrypt.compare(password, user.password) // Compare the password hashes
+        if (matched) {
+            res.status(200).json({
+                status: 'success'
+            })
+        } else {
+            res.status(400).json({
+                status: 'failed',
+                error: 'Password incorrect'
+            })
+        }
+    } catch (err) {
+        res.status(500).json({
+            status: 'failed',
+            error: err
+        })
+    }
+}
--- index.js
+++ index.js
@@ -2,11 +2,14 @@ const express = require('express')
 const mongoose = require('mongoose')
 const dbConfig = require('./config/config')
 
-const app = express()
-const port = process.env.PORT || 3000
+const postRouter = require('./routes/postRoutes')
+const userRouter = require('./routes/userRoutes')
 
+const app = express()
 
 const dbUrl = `mongodb://${dbConfig.MONGO_USER}:${dbConfig.MONGO_PASSWORD}@${dbConfig.MONGO_IP}:${dbConfig.MONGO_PORT}/?authSource=admin`
+
+console.log(dbUrl)
 
 // Attempt to connect to database up to 20 times with exponential backoff
 const connectWithRetry = (retry = 0, delay = 100) => {
@@ -15,16 +18,22 @@ const connectWithRetry = (retry = 0, delay = 100) => {
         .catch((err) => {
             console.log(`Retry: ${retry} Unexpected error connecting to db: ${err}`)
 
-            if (retry < 20) setTimeout(connectWithRetry(retry++, delay ** 2), delay)
+            if (retry < 20) {
+                setTimeout(connectWithRetry(retry++, delay ** 2), delay)
+            }
         })
 }
 
 connectWithRetry()
 
-
+app.use(express.json()) //Ensures body is attached to the request object
 
 app.get('/', (req, res) => {
     res.send("<h1>Hey!! Updated again</h1>")
 })
 
+app.use('/api/v1/posts', postRouter)
+app.use('/api/v1/users', userRouter)
+
+const port = process.env.PORT || 3000
 app.listen(port, () => console.log(`Listening on port ${port}`))
