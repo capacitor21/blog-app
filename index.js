fac91b4545f55833110ed1cac522a307163d4a43
Michael DeDreu <mdedreu21@gmail.com>
2022-01-01 22:32:57 -0500

    Setup mongo container and connection between the app and database

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const express = require('express')
+const mongoose = require('mongoose')
 
 const app = express()
-
 const port = process.env.PORT || 3000
+
+mongoose.connect('mongodb://root:docker123@db/?authSource=admin')
+    .then(() => console.log('Successfully connected to the database'))
+    .catch((err) => console.log(`Unexpected error connecting to db: ${err}`))
 
 app.get('/', (req, res) => {
     res.send("<h1>Hey!! Updated again</h1>")
